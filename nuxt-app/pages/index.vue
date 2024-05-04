46a524118c612b68c0a805ca22226e6daad5c873
Bruno Teixeira <bteixeira666@gmail.com>
2024-05-04 19:05:41 +0200

    Replaced pagination with use of Vuetify component.

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -3,8 +3,8 @@
 import type {PaginatedProjects} from '~/types/project'
 
 type Params = {
-    pageSize?: number
-    page?: number
+    pageSize?: number | string
+    page?: number | string
     updatedSince?: string // Date
 }
 
@@ -60,23 +60,66 @@ const buildLink = (params: Params) => {
         </span>
 
     </div>
-    <div>
-        The current page is:
-        <pre style="border: solid 1px gray; display: inline-block">{{ page }}</pre>
-    </div>
     <div v-if="pending">
         Loading ...
     </div>
     <div v-else>
         <p>
             (total {{ data?.totalCount }} projects in {{ totalPages }} pages)
-            Page:
-            <span v-for="n in totalPages">
-                <NuxtLink :to="buildLink({page: n})">{{ n }}</NuxtLink>&nbsp;
-            </span>
+            <v-pagination :length="totalPages" show-first-last-page density="compact" :model-value="page">
+                <template v-slot:first="props">
+                    <v-btn :to="buildLink({page: 1})" nuxt :disabled="props.disabled" variant="text" density="compact" icon :active="false">
+                        <v-icon icon="mdi-page-first"/>
+                    </v-btn>
+                </template>
+                <template v-slot:prev="props">
+                    <v-btn :to="buildLink({page: page - 1})" nuxt :disabled="props.disabled" variant="text" density="compact" icon :active="false">
+                        <v-icon icon="mdi-chevron-left"/>
+                    </v-btn>
+                </template>
+
+                <template v-slot:item="item">
+                    <div v-if="item.isActive" class="pagination-link -active">
+                        {{ item.page }}
+                    </div>
+                    <NuxtLink v-else :to="buildLink({page: item.page})" class="pagination-link">
+                        {{ item.page }}
+                    </NuxtLink>
+                </template>
+
+                <template v-slot:next="props">
+                    <v-btn :to="buildLink({page: page + 1})" nuxt :disabled="props.disabled" variant="text" density="compact" icon :active="false">
+                        <v-icon icon="mdi-chevron-right"/>
+                    </v-btn>
+                </template>
+                <template v-slot:last="props">
+                    <v-btn :to="buildLink({page: totalPages})" nuxt :disabled="props.disabled" variant="text" density="compact" icon :active="false">
+                        <v-icon icon="mdi-page-last"/>
+                    </v-btn>
+                </template>
+            </v-pagination>
         </p>
 
         <ProjectList :projects="data?.projects || []"/>
     </div>
     Footer
 </template>
+
+<style scoped>
+.pagination-link {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 1.75em;
+    height: 1.75em;
+    border-radius: 4px;
+
+    &:hover {
+        background-color: #f4f4f4;
+    }
+
+    &.-active {
+        background-color: #ddd;
+    }
+}
+</style>
